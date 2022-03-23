961a2b8409714a30c79ed92576f9a19553ab0863
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-03-23 11:10:04 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react';
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Ama");
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const blog ={title, body, author};
+console.log(blog);
+
+  }
+
+  return (
+  <div className='create'>
+  <h2>Add A New Blog</h2>
+  <form onSubmit={handleSubmit}>
+  <label>Blog Title:</label>
+  <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
+
+  <label>Blog Body:</label>
+  <textarea required value={body} onChange={(e)=>setBody(e.target.value)}></textarea>
+
+<label>Blog Author</label>
+<select value={author} onChange={(e)=>setAuthor(e.target.value)}>
+<option value="Ama">Ama</option>
+<option value="Kofi">Kofi</option>
+<option value="Yaw">Yaw</option>
+
+
+
+</select>
+
+<button>Add Blog</button>
+
+  </form>
+  
+  </div>
+  )
+}
+
+export default App
